076e74af8cae90e2b1c48881e019d78340f779fc
Ariane Mateus <ariane.ccomp@gmail.com>
2020-05-22 09:54:12 -0300

    refactor: upload file

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -25,9 +25,9 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
-  category: Category;
+  category: Category; 
 
   @Column()
   category_id: string;
